a57986048d472503f6294d6f21acb1dd347768ea
Nicholas Gimbel <56981761+ngimb64@users.noreply.github.com>
2022-04-14 20:22:16 -0400

    Update - 4/14/22

--- TwoIps_SameNetwork2.0/InputMod.py
+++ TwoIps_SameNetwork2.0/InputMod.py
@@ -3,38 +3,36 @@ def main_input(message):
         try:
             user_input = int(input(message))
             if user_input < 000:
-                print('Please enter a number between' \
-                        ' 000 and 255.')
+                print('Please enter a number between 000 and 255.')
                 continue
             elif user_input > 255:
-                print('Please enter a number between' \
-                        ' 000 and 255.')
+                print('Please enter a number between 000 and 255.')
                 continue
+
+            break
+
         except ValueError:
-            print('Please enter a number between' \
-                    ' 000 and 255.')
-            continue
-        else:
-            return user_input
+            print('Please enter a number between 000 and 255.')
+
+    return user_input
 
 def alt_input(message):
     while True:
         try:
             user_input = int(input(message))
             if user_input < 000:
-                print('Please enter a number between' \
-                        ' 000 and 254.')
+                print('Please enter a number between 000 and 255.')
                 continue
             elif user_input > 254:
-                print('Please enter a number between' \
-                        ' 000 and 254.')
+                print('Please enter a number between 000 and 255.')
                 continue
+
+            break
+
         except ValueError:
-            print('Please enter a number between' \
-                    ' 000 and 254.')
-            continue
-        else:
-            return user_input
+            print('Please enter a number between 000 and 255.')
+
+    return user_input
 
 def sub_input(message):
     while True:
@@ -43,28 +41,23 @@ def sub_input(message):
             if not user_input == 255:
                 print('Please enter 255.')
                 continue
+            else:
+                return user_input
+
         except ValueError:
             print('Please enter 255.')
-            continue
-        else:
-            return user_input
 
 def cidr_input(message):
-    options = [0, 128, 192, 224, 240, 248, \
-                252, 254, 255]
+    options = [0, 128, 192, 224, 240, 248, 252, 254, 255]
+
     while True:
         try:
             user_input = int(input(message))
             if user_input in options:
                 return user_input
-                break
             else:
-                print('Please enter one of the' \
-                        ' specified values.')
+                print('Please enter one of the specified values.')
                 continue
+
         except ValueError:
-            print('Please enter one of the' \
-                    'specified values.')
-            continue
-        else:
-            return user_input
+            print('Please enter one of the specified values.')
